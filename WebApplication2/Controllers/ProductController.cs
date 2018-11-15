0afc118497237a2a50183ede62b7f84502fa915c
Federico <Federico@DESKTOP-TN8UH77>
2018-11-15 16:34:28 -0300

    Added product listing view

--- WebApplication2/Controllers/ProductController.cs
+++ WebApplication2/Controllers/ProductController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class ProductController : Controller
     {
+        ProductsDBAdapter productsAdapter = new ProductsDBAdapter();
+
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View(productsAdapter.Products);
         }
 
         // GET: Product/Details/5
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class ProductsDBAdapter
+    {
+        private string connectionString;
+
+        public ProductsDBAdapter()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+        }
+
+        public IList<Product> Products
+        {
+            get
+            {
+                List<Product> result = new List<Product>();
+                Product p = null;
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand comm = conn.CreateCommand())
+                    {
+                        conn.Open();
+                        comm.CommandText = "SELECT * FROM PRODUCTS";
+                        using (SqlDataReader reader = comm.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while(reader.Read())
+                                {
+                                    p = new Product();
+                                    p.Id = reader.GetInt32(0);
+                                    p.Name = reader.GetString(1);
+                                    p.Stock = reader.GetInt32(2);
+                                    p.Description = reader.GetString(3);
+
+                                    result.Add(p);
+                                }
+                            }
+                            reader.Close();
+                        }
+                    }
+                    conn.Close();
+
+                    return result;
+                }
+
+            }
+        }
+
+    }
+}
